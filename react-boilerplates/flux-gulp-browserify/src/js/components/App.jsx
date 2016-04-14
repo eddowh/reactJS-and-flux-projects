064777caa08d3769f8cec191b6380a2ed555a25d
Eddo Hintoso <eddo.hintoso@gmail.com>
2016-04-14 15:08:22 +0800

    Improve FGB (flux-gulp-browserify) boilerplate

--- react-boilerplates/flux-gulp-browserify/src/js/components/App.jsx
+++ react-boilerplates/flux-gulp-browserify/src/js/components/App.jsx
@@ -2,12 +2,42 @@
  * Main application
  */
 
+
 var React = require('react');
 
+// Flux elements
 var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
 
+// Components
+// var SubComponent = require('./SubComponent.jsx');
+
+
+function getAppState() {
+    return {
+
+    };
+}
+
+
 var App = React.createClass({
+
+    getInitialState: function() {
+        return getAppState();
+    },
+
+    componentDidMount: function() {
+        AppStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+        AppStore.removeChangeListener(this._onChange);
+    },
+
+    // Update view state when change is received
+    _onChange: function() {
+        this.setState(getAppState());
+    },
 
     render: function() {
         return (
--- react-boilerplates/flux-gulp-browserify/src/js/utils/appAPI.js
+++ react-boilerplates/flux-gulp-browserify/src/js/utils/appAPI.js
@@ -2,7 +2,7 @@ var AppActions = require('../actions/AppActions');
 
 // list of actions that are available
 // i.e. `searchMovies(movie)` for a movie finding app
-// name of the function must be identical to those defined in `AppActions`
+// name of the function should preferably be identical to those defined in `AppActions`
 module.exports = {
 
 }
